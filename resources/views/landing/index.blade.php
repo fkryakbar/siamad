253427f4a287902e4c0c7a005bf7b97d8df0ce0b
fkrydev <fikri.afa289@gmail.com>
2023-08-12 20:33:06 +0800

    fix typo

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -79,7 +79,7 @@
                 <!-- Modal footer -->
                 <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
                     <button data-modal-hide="defaultModal" type="button"
-                        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">I
+                        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                         Kembali</button>
                 </div>
             </div>
