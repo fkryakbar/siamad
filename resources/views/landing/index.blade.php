608503bc864d4c7090b79682c3ece1cba9adee96
fkrydev <fikri.afa289@gmail.com>
2023-08-12 20:30:42 +0800

    add lupa password modal

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -25,17 +25,18 @@
                         <div class="mb-3">
                             <label for="username" class="block mb-2 text-sm font-medium text-gray-900 ">Username</label>
                             <input type="text" id="username" placeholder="Masukkan username" name="username"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-green-500 active:outline-green-500 active:ring-blue-500 block w-full p-2.5">
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:outline-green-500 active:outline-green-500 active:ring-green-500 block w-full p-2.5">
                         </div>
                         <div class="mb-3">
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
                             <input type="password" id="password" placeholder="Masukkan password" name="password"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-green-500 active:outline-green-500 active:ring-blue-500 block w-full p-2.5">
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:outline-green-500 active:outline-green-500 active:ring-green-500 block w-full p-2.5">
                         </div>
                         <div class="mb-3 flex justify-between items-center">
                             <button type="submit"
                                 class="bg-green-500 font-semibold p-2 rounded-lg hover:bg-green-700 transition-all w-28 drop-shadow active:scale-105 text-white text-sm">Masuk</button>
-                            <a class=" text-green-500 hover:text-green-700 text-sm" href="">Lupa
+                            <a data-modal-target="defaultModal" data-modal-toggle="defaultModal"
+                                class="cursor-pointer text-green-500 hover:text-green-700 text-sm">Lupa
                                 Password?</a>
                         </div>
                         <div class="flex justify-center">
@@ -48,4 +49,41 @@
             </div>
         </div>
     </section>
+    <div id="defaultModal" tabindex="-1" aria-hidden="true"
+        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-2xl max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow">
+                <!-- Modal header -->
+                <div class="flex items-start justify-between p-4 border-b rounded-t ">
+                    <h3 class="text-xl font-semibold text-gray-900 ">
+                        Lupa Password
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center "
+                        data-modal-hide="defaultModal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                    <p class="text-base leading-relaxed text-gray-500 ">
+                        Jika lupa password silahkan hubungi administrator
+                    </p>
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
+                    <button data-modal-hide="defaultModal" type="button"
+                        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">I
+                        Kembali</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
