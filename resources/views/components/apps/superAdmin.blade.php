0fedec2e8594e38205d65b968962c75a90592f5a
fkrydev <fikri.afa289@gmail.com>
2024-01-25 09:08:15 +0800

    update akademik link

--- resources/views/components/apps/superAdmin.blade.php
+++ resources/views/components/apps/superAdmin.blade.php
@@ -1,9 +1,9 @@
 <div class="bg-white shadow rounded-lg p-5 mt-2 flex justify-between items-center">
     <div>
-        <p class="font-semibold text-sm text-gray-500">Akademik</p>
-        <p class="text-lg  font-semibold">Portal Akademik Admin</p>
+        <p class="font-semibold text-sm text-gray-500">Admin</p>
+        <p class="text-lg  font-semibold">SIA - Sistem Informasi Akademik STIT Assunniyyah Tambarangan</p>
     </div>
-    <a href="https://portal-admin.stitastbr.ac.id"
+    <a href="https://akademik.stitastbr.ac.id"
         class="bg-gray-100 rounded-full p-4 w-36 text-center font-semibold hover:bg-gray-300 transition-all">
         Buka
     </a>
